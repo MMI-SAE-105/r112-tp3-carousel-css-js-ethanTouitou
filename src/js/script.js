e6da03205fd83515f28c3fec59ba94545a47451d
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2024-11-19 16:12:11 +0100

    Add carousel functionality to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 // *** CAROUSEL ***
 // TODO
+const carousel = document.querySelector(".carouselcontainer");
+const prevButton = document.querySelector(".carouselbutton--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+
+if (carousel) {
+
+    prevButton.addEventListener("click", () => {
+        carousel.scrollBy({ left: -260, behavior: "smooth" });
+    });
+
+
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({ left: 260, behavior: "smooth" });
+    });
+}
